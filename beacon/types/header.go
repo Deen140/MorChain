952bd86b39a77b6d4ac4b49ce6410da3578b2ada
reinhardketh <97641006+reinhardketh@users.noreply.github.com>
2023-07-29 22:52:26 +0800

    Update header.go

--- beacon/types/header.go
+++ beacon/types/header.go
@@ -1,18 +1,18 @@
-// Copyright 2022 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// Copyright 2022 The go-morchain Authors
+// This file is part of the go-morchain library.
 //
-// The go-ethereum library is free software: you can redistribute it and/or modify
+// The go-morchain library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// The go-ethereum library is distributed in the hope that it will be useful,
+// The go-morchain library is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU Lesser General Public License for more details.
 //
 // You should have received a copy of the GNU Lesser General Public License
-// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
+// along with the go-morchain library. If not, see <http://www.gnu.org/licenses/>.
 
 // Package types implements a few types of the beacon chain for light client usage.
 package types
@@ -21,9 +21,9 @@ import (
 	"crypto/sha256"
 	"encoding/binary"
 
-	"github.com/ethereum/go-ethereum/beacon/merkle"
-	"github.com/ethereum/go-ethereum/beacon/params"
-	"github.com/ethereum/go-ethereum/common"
+	"github.com/morchain/go-morchain/beacon/merkle"
+	"github.com/morchain/go-morchain/beacon/params"
+	"github.com/morchain/go-morchain/common"
 )
 
 //go:generate go run github.com/fjl/gencodec -type Header -field-override headerMarshaling -out gen_header_json.go
@@ -39,7 +39,7 @@ const (
 // Header defines a beacon header.
 //
 // See data structure definition here:
-// https://github.com/ethereum/consensus-specs/blob/dev/specs/phase0/beacon-chain.md#beaconblockheader
+// https://github.com/morchain/consensus-specs/blob/dev/specs/phase0/beacon-chain.md#beaconblockheader
 type Header struct {
 	// Monotonically increasing slot number for the beacon block (may be gapped)
 	Slot uint64 `gencodec:"required" json:"slot"`
@@ -50,10 +50,10 @@ type Header struct {
 	// SSZ hash of the parent beacon header
 	ParentRoot common.Hash `gencodec:"required" json:"parent_root"`
 
-	// SSZ hash of the beacon state (https://github.com/ethereum/consensus-specs/blob/dev/specs/bellatrix/beacon-chain.md#beacon-state)
+	// SSZ hash of the beacon state (https://github.com/morchain/consensus-specs/blob/dev/specs/bellatrix/beacon-chain.md#beacon-state)
 	StateRoot common.Hash `gencodec:"required" json:"state_root"`
 
-	// SSZ hash of the beacon block body (https://github.com/ethereum/consensus-specs/blob/dev/specs/bellatrix/beacon-chain.md#beaconblockbody)
+	// SSZ hash of the beacon block body (https://github.com/morchain/consensus-specs/blob/dev/specs/bellatrix/beacon-chain.md#beaconblockbody)
 	BodyRoot common.Hash `gencodec:"required" json:"body_root"`
 }
 
@@ -106,7 +106,7 @@ func SyncPeriod(slot uint64) uint64 {
 // SignedHeader represents a beacon header signed by a sync committee.
 //
 // This structure is created from either an optimistic update or an instant update:
-//   - https://github.com/ethereum/consensus-specs/blob/dev/specs/altair/light-client/sync-protocol.md#lightclientoptimisticupdate
+//   - https://github.com/morchain/consensus-specs/blob/dev/specs/altair/light-client/sync-protocol.md#lightclientoptimisticupdate
 //   - https://github.com/zsfelfoldi/beacon-APIs/blob/instant_update/apis/beacon/light_client/instant_update.yaml
 type SignedHeader struct {
 	// Beacon header being signed
