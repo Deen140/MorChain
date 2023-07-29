0368384e3457410d039b80da16f12711df89b1b7
reinhardketh <97641006+reinhardketh@users.noreply.github.com>
2023-07-29 23:25:41 +0800

    Update cmd.go

--- cmd/utils/cmd.go
+++ cmd/utils/cmd.go
@@ -1,20 +1,20 @@
-// Copyright 2014 The go-ethereum Authors
-// This file is part of go-ethereum.
+// Copyright 2014 The go-morchain Authors
+// This file is part of go-morchain.
 //
-// go-ethereum is free software: you can redistribute it and/or modify
+// go-morchain is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// go-ethereum is distributed in the hope that it will be useful,
+// go-morchain is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with go-ethereum. If not, see <http://www.gnu.org/licenses/>.
+// along with go-morchain. If not, see <http://www.gnu.org/licenses/>.
 
-// Package utils contains internal helper functions for go-ethereum commands.
+// Package utils contains internal helper functions for go-morchain commands.
 package utils
 
 import (
@@ -30,17 +30,17 @@ import (
 	"syscall"
 	"time"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core"
-	"github.com/ethereum/go-ethereum/core/rawdb"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/ethereum/go-ethereum/eth/ethconfig"
-	"github.com/ethereum/go-ethereum/ethdb"
-	"github.com/ethereum/go-ethereum/internal/debug"
-	"github.com/ethereum/go-ethereum/log"
-	"github.com/ethereum/go-ethereum/node"
-	"github.com/ethereum/go-ethereum/rlp"
+	"github.com/morchain/go-morchain/common"
+	"github.com/morchain/go-morchain/core"
+	"github.com/morchain/go-morchain/core/rawdb"
+	"github.com/morchain/go-morchain/core/types"
+	"github.com/morchain/go-morchain/crypto"
+	"github.com/morchain/go-morchain/eth/ethconfig"
+	"github.com/morchain/go-morchain/ethdb"
+	"github.com/morchain/go-morchain/internal/debug"
+	"github.com/morchain/go-morchain/log"
+	"github.com/morchain/go-morchain/node"
+	"github.com/morchain/go-morchain/rlp"
 	"github.com/urfave/cli/v2"
 )
 
