c494315acabe753fdf686cfcb3f0f14fe44f52ac
reinhardketh <97641006+reinhardketh@users.noreply.github.com>
2023-07-29 23:04:59 +0800

    Update crawl.go

--- cmd/devp2p/crawl.go
+++ cmd/devp2p/crawl.go
@@ -1,18 +1,18 @@
-// Copyright 2019 The go-ethereum Authors
-// This file is part of go-ethereum.
+// Copyright 2019 The go-morchain Authors
+// This file is part of go-morchain.
 //
-// go-ethereum is free software: you can redistribute it and/or modify
+// go-morchain is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// go-ethereum is distributed in the hope that it will be useful,
+// go-morchain is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with go-ethereum. If not, see <http://www.gnu.org/licenses/>.
+// along with go-morchain. If not, see <http://www.gnu.org/licenses/>.
 
 package main
 
@@ -21,8 +21,8 @@ import (
 	"sync/atomic"
 	"time"
 
-	"github.com/ethereum/go-ethereum/log"
-	"github.com/ethereum/go-ethereum/p2p/enode"
+	"github.com/morchain/go-morchain/log"
+	"github.com/morchain/go-morchain/p2p/enode"
 )
 
 type crawler struct {
