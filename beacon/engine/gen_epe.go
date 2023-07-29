be759efa10f4f06b14b3d8eed8a789b7b737fde3
reinhardketh <97641006+reinhardketh@users.noreply.github.com>
2023-07-29 22:49:57 +0800

    Update gen_epe.go

--- beacon/engine/gen_epe.go
+++ beacon/engine/gen_epe.go
@@ -7,7 +7,7 @@ import (
 	"errors"
 	"math/big"
 
-	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/morchain/go-morchain/common/hexutil"
 )
 
 var _ = (*executionPayloadEnvelopeMarshaling)(nil)
