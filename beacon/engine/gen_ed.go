d9eac76e0319cddeaea0f7b1fe42a9a60c8e1cd8
reinhardketh <97641006+reinhardketh@users.noreply.github.com>
2023-07-29 22:49:45 +0800

    Update gen_ed.go

--- beacon/engine/gen_ed.go
+++ beacon/engine/gen_ed.go
@@ -7,9 +7,9 @@ import (
 	"errors"
 	"math/big"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/hexutil"
-	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/morchain/go-morchain/common"
+	"github.com/morchain/go-morchain/common/hexutil"
+	"github.com/morchain/go-morchain/core/types"
 )
 
 var _ = (*executableDataMarshaling)(nil)
